367889932f966af68325ed30370dcf5688c08943
William Duyck <fuzzyfox0@gmail.com>
2014-06-11 19:48:40 +0100

    race protection

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var morgan = require( 'morgan' );
 var env = require( 'nconf' ).argv()
                             .env()
                             .file( { file: 'config.json' } );
+var sass = require( 'node-sass' );
+var fs = require( 'fs' );
+
 // setup server
 var app = express();
 
@@ -13,6 +16,9 @@ var healthcheck = {
   version: require('./package').version,
   http: 'okay'
 };
+
+// pending processes
+var pending = [];
 
 // get some debug ouptut
 if( env.get( 'debug' ) || env.get( 'DEBUG' ) ) {
@@ -24,12 +30,26 @@ app.get( [ '/', 'healthcheck' ], function( req, res ) {
 	res.jsonp( healthcheck );
 });
 
+// function to help deal w/ race conditions
+function waitOnTemp( hash, done ) {
+	if( pending.indexOf( hash ) > -1 ) {
+		setTimeout( function(){
+			waitOnTemp( hash, done );
+		}, 100 );
+		return;
+	}
+
+	// file written, read it and send it
+	var result = fs.readFileSync( hash + '.css', 'utf-8' );
+	done( result );
+}
+
 // get sass variables from url
 app.post( '/compile', function( req, res ) {
 	var result = ''; // this will become the compiled css.
 
 	// check that the request came from a trusted source ( not foolproof )
-	if( env.get( 'SHARED_SECRET' ) !== res.param( 'secret' ) ) {
+	if( env.get( 'SHARED_SECRET' ) !== req.param( 'secret' ) ) {
 		res.status( 400 ).jsonp({
 			errors: [{
 				message: 'Request requires authentication.',
@@ -38,10 +58,33 @@ app.post( '/compile', function( req, res ) {
 		});
 	}
 
+	// deal w/ race conditions
+	if( pending.indexOf( req.param( 'hash' ) ) > -1 ) {
+		// wait for temp file w/ css to exist
+		return waitOnTemp( req.param( 'hash' ), function( result ) {
+			res.send( result );
+		});
+	}
+
+	// set hash as pending
+	pending.push( req.param( 'hash' ) );
+
 	// magical shite here
+
+	/*
+		- get default variables for the theme
+		- generate overrides for this hash
+		- generate import for rest of theme
+		- write a temp-file to compile
+		- return the compiled output to user ( hint: use result )
+	 */
 
 	// send back the css
 	res.send( result );
+
+	// clear hash from pending queue
+	var pendingIdx = pending.indexOf( req.param( 'hash' ) );
+	pending.splice( pendingIdx, 1 );
 });
 
 // spin up the server
